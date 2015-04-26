e83a34ade64c300cf39d49eb9a41bb5222a1c909
Iori Mizutani <iori.mizutani@gmail.com>
2015-04-26 21:17:36 +0900

    Fix SECOND_FIELDS instantiation to hold epc, oid, and prop properly

--- fc-server/src/main/java/org/fosstrak/ale/server/Pattern.java
+++ fc-server/src/main/java/org/fosstrak/ale/server/Pattern.java
@@ -36,80 +36,95 @@ public class Pattern {
 	/** content of the first field */
 	private static final String FIRST_FIELD = "urn";
 	/** content of the second field */
-	private static final String[] SECOND_FIELD = new String[] {"epc", "oid", "prop"};
+	private static final String[] SECOND_FIELDS = new String[] { "epc", "oid",
+			"prop" };
 	/** possible contents of the third field */
-	private static final String[] THIRD_FIELDS = new String[] {"urn", "tag", "pat", "id", "idpat", "raw"};
-	private static final String[] ISOs = new String[]{"1.0.17363", "1.0.17365"};
-	private static final String OID = "oid";
-	
+	private static final String[] THIRD_FIELDS = new String[] { "urn", "tag",
+			"pat", "id", "idpat", "raw" };
+	@SuppressWarnings("unused")
+	private static final String[] ISOs = new String[] { "1.0.17363",
+			"1.0.17365" };
+
 	/** how this pattern is used (tag, filter or group) */
 	private final PatternUsage usage;
-	/** conent of third field */
+	/** Content of second field */
+	private String secondField;
+	/** Content of third field */
 	private String thirdField;
 	/** type of this pattern */
 	private PatternType type;
-	
+
 	/** data fields of this pattern */
 	private final List<PatternDataField> dataFields = new ArrayList<PatternDataField>();
-	
+
 	/**
-	 * Contructor sets the usage and parses the pattern.
+	 * Constructor sets the usage and parses the pattern.
 	 * 
 	 * @param pattern to parse
 	 * @param usage of this pattern
 	 * @throws ECSpecValidationException if the pattern is invalid
 	 */
-	public Pattern(String pattern, PatternUsage usage) throws ECSpecValidationException {
+	public Pattern(String pattern, PatternUsage usage)
+			throws ECSpecValidationException {
 		// set fields
 		this.usage = usage;
-		
+
 		StringBuffer thirdFieldString = new StringBuffer();
 		thirdFieldString.append("(");
 		for (int i = 0; i < THIRD_FIELDS.length; i++) {
-			if (i > 0) thirdFieldString.append(" | ");
+			if (i > 0)
+				thirdFieldString.append(" | ");
 			thirdFieldString.append(THIRD_FIELDS[i]);
 		}
 		thirdFieldString.append(")");
 		StringBuffer secondFieldString = new StringBuffer();
 		secondFieldString.append("(");
-		for (int i = 0; i < SECOND_FIELD.length; i++) {
-			if (i > 0) secondFieldString.append(" | ");
-			secondFieldString.append(SECOND_FIELD[i]);
+		for (int i = 0; i < SECOND_FIELDS.length; i++) {
+			if (i > 0)
+				secondFieldString.append(" | ");
+			secondFieldString.append(SECOND_FIELDS[i]);
 		}
 		secondFieldString.append(")");
-		
+
 		// split pattern and check first fields
 		String[] parts = pattern.split(":");
-		if (parts[1]=="epc" & parts.length != 5) {
-			throw new ECSpecValidationException("Invalid Pattern '" + pattern + "'." +
-					" Pattern must have the form '" + FIRST_FIELD + ":" + secondFieldString + ":" + thirdFieldString + ":tag-type:data-fields'.");
+		if (parts.length != 5) {
+			if (parts.length < 2 || parts[1] == "epc" ) {
+				throw new ECSpecValidationException("Invalid Pattern '" + pattern
+						+ "'." + " Pattern must have the form '" + FIRST_FIELD
+						+ ":" + secondFieldString + ":" + thirdFieldString
+						+ ":tag-type:data-fields'.");
+			}
 		} else {
 			thirdField = parts[2];
-//			if (!FIRST_FIELD.equals(parts[0]) || !SECOND_FIELD.equals(parts[1]) || !containsString(THIRD_FIELDS, thirdField)) {
-			//if (!FIRST_FIELD.equals(parts[0]) || !containsString(SECOND_FIELD,parts[1]) || !containsString(THIRD_FIELDS, thirdField)) {
-		    if (!FIRST_FIELD.equals(parts[0]) || !containsString(SECOND_FIELD,parts[1]) ) {
-				throw new ECSpecValidationException("Invalid Pattern '" + pattern + "'." +
-						" Pattern must start with '" + FIRST_FIELD + ":" + secondFieldString + ":" + thirdFieldString + "'.");
+			// if (!FIRST_FIELD.equals(parts[0]) ||
+			// !SECOND_FIELD.equals(parts[1]) || !containsString(THIRD_FIELDS,
+			// thirdField)) {
+			// if (!FIRST_FIELD.equals(parts[0]) ||
+			// !containsString(SECOND_FIELD,parts[1]) ||
+			// !containsString(THIRD_FIELDS, thirdField)) {
+			if (!FIRST_FIELD.equals(parts[0])
+					|| !containsString(SECOND_FIELDS, parts[1])) {
+				throw new ECSpecValidationException("Invalid Pattern '"
+						+ pattern + "'." + " Pattern must start with '"
+						+ FIRST_FIELD + ":" + secondFieldString + ":"
+						+ thirdFieldString + "'.");
 			} else {
-				
+				secondField = parts[1];
 				// get pattern type
-				if (OID.equals(parts[1])) {
+				if (secondField.equals("oid")) {
 					type = PatternType.getType(parts[1]);
-					//parseDataFields(parts[3],pattern); 
-				}
-				else if (parts[1].equals("epc")) {
+					// parseDataFields(parts[3],pattern);
+				} else if (secondField.equals("epc")) {
 					type = PatternType.getType(parts[3]);
-					parseDataFields(parts[4],pattern); 
-				}
-				else if (parts[1].equals("prop")) type = PatternType.getType(parts[1]);
-				
-				
+					parseDataFields(parts[4], pattern);
+				} else if (secondField.equals("prop"))
+					type = PatternType.getType(parts[1]);
 				// parse data fields
-				//parseDataFields(parts[4], pattern);
-				
+				// parseDataFields(parts[4], pattern);
 			}
 		}
-		
+
 	}
 
 	/**
@@ -118,55 +133,55 @@ public class Pattern {
 	 * @return type of pattern
 	 */
 	public PatternType getType() {
-		
+
 		return type;
-		
+
 	}
-	
+
 	/**
 	 * This method returns the filter of this pattern.
 	 * 
 	 * @return filter of pattern
 	 */
 	public PatternDataField getFilter() {
-		
+
 		return dataFields.get(0);
-		
+
 	}
-	
+
 	/**
 	 * This method returns the company of this pattern.
 	 * 
 	 * @return company of pattern
 	 */
 	public PatternDataField getCompany() {
-		
+
 		return dataFields.get(1);
-		
+
 	}
-	
+
 	/**
 	 * This method returns the item of this pattern.
 	 * 
 	 * @return item of pattern
 	 */
 	public PatternDataField getItem() {
-		
+
 		return dataFields.get(2);
-		
+
 	}
-	
+
 	/**
 	 * This method returns the serial of this patern.
 	 * 
 	 * @return serial of pattern
 	 */
 	public PatternDataField getSerial() {
-		
+
 		return dataFields.get(3);
-		
+
 	}
-	
+
 	/**
 	 * This method returns the data fields of this pattern.
 	 * 
@@ -175,196 +190,226 @@ public class Pattern {
 	public List<PatternDataField> getDataFields() {
 		return dataFields;
 	}
-	
+
 	/**
-	 * This method indicates if this pattern is disjoint to the specified pattern.
+	 * This method indicates if this pattern is disjoint to the specified
+	 * pattern.
 	 * 
-	 * @param pattern to check disjointness
+	 * @param pattern
+	 *            to check disjointness
 	 * @return true if the patterns are disjoint and false otherwise
-	 * @throws ECSpecValidationException if the pattern is invalid
+	 * @throws ECSpecValidationException
+	 *             if the pattern is invalid
 	 */
 	public boolean isDisjoint(String pattern) throws ECSpecValidationException {
 		return isDisjoint(new Pattern(pattern, PatternUsage.GROUP));
 	}
-	
+
 	/**
-	 * This method indicates if this pattern is disjoint to the specified pattern.
+	 * This method indicates if this pattern is disjoint to the specified
+	 * pattern.
 	 * 
-	 * @param pattern to chek disjointness
+	 * @param pattern
+	 *            to chek disjointness
 	 * @return true if the patterns are disjoint and false otherwise
-	 * @throws ECSpecValidationException the pattern cannot be validated.
+	 * @throws ECSpecValidationException
+	 *             the pattern cannot be validated.
 	 */
 	public boolean isDisjoint(Pattern pattern) throws ECSpecValidationException {
 		if (!type.equals(pattern.getType())) {
-			
+
 			// if types are different, then the patterns are disjoint
 			return true;
 		} else {
-			
-			// if some corresponding data fields are disjoint, then the patterns are disjoint 
+
+			// if some corresponding data fields are disjoint, then the patterns
+			// are disjoint
 			for (int i = 0; i < dataFields.size(); i++) {
-				if (dataFields.get(i).isDisjoint(pattern.getDataFields().get(i))) {
+				if (dataFields.get(i)
+						.isDisjoint(pattern.getDataFields().get(i))) {
 					return true;
 				}
 			}
 			return false;
 		}
-		 
+
 	}
-	
+
 	/**
 	 * This method indicates if a tag is member of this filter or group pattern.
 	 * If the pattern is a tag pattern, the return value is null.
 	 * 
-	 * @param tagURI to check for member
+	 * @param tagURI
+	 *            to check for member
 	 * @return true if tag is member of this pattern and false otherwise
-	 * @throws ECSpecValidationException if the tag pattern is invalid
-	 * @throws ImplementationException if an implementation exception occurs
+	 * @throws ECSpecValidationException
+	 *             if the tag pattern is invalid
+	 * @throws ImplementationException
+	 *             if an implementation exception occurs
 	 */
-	public boolean isMember(String tagURI) throws ECSpecValidationException, ImplementationException {
-		
+	public boolean isMember(String tagURI) throws ECSpecValidationException,
+			ImplementationException {
+
 		if (usage == PatternUsage.TAG) {
 			return false;
 		}
-		
+
 		// create pattern of usage TAG ('*' and 'X' are not allowed)
 		Pattern tag = new Pattern(tagURI, PatternUsage.TAG);
-			
+
 		// check type
 		if (tag.getType().equals(getType())) {
-				
+
 			// get data fields
 			List<PatternDataField> tagDataFields = tag.getDataFields();
-				
+
 			// check number of data fields
 			if (tagDataFields.size() == dataFields.size()) {
-				
+
 				// check contents of data fields
 				for (int i = 0; i < dataFields.size(); i++) {
-					if (!dataFields.get(i).isMember(tagDataFields.get(i).getValue())) {
+					if (!dataFields.get(i).isMember(
+							tagDataFields.get(i).getValue())) {
 						return false;
 					}
 				}
 				return true;
-					
+
 			}
 		}
-		
+
 		return false;
-		
+
 	}
 
 	/**
-	 * This method returns the group name for a tag depending on this group pattern.
-	 * If the pattern is not a group pattern or the tag is not a member of this group pattern,
-	 * the return value is null. 
+	 * This method returns the group name for a tag depending on this group
+	 * pattern. If the pattern is not a group pattern or the tag is not a member
+	 * of this group pattern, the return value is null.
 	 * 
-	 * @param tagURI to get the group name from
+	 * @param tagURI
+	 *            to get the group name from
 	 * @return group name
-	 * @throws ImplementationException if an implementation exception occurs
-	 * @throws ECSpecValidationException if the tag pattern is invalid
+	 * @throws ImplementationException
+	 *             if an implementation exception occurs
+	 * @throws ECSpecValidationException
+	 *             if the tag pattern is invalid
 	 */
-	public String getGroupName(String tagURI) throws ImplementationException, ECSpecValidationException {
-		
+	public String getGroupName(String tagURI) throws ImplementationException,
+			ECSpecValidationException {
+
 		if (usage != PatternUsage.GROUP || !isMember(tagURI)) {
 			return null;
 		}
-		
+
 		try {
-			
+
 			// create pattern of usage TAG ('*', 'X' and ranges are not allowed)
 			Pattern tag = new Pattern(tagURI, PatternUsage.TAG);
-			
+
 			// clone pattern to create a group name
 			Pattern groupName = new Pattern(this.toString(), PatternUsage.GROUP);
-			
+
 			// get data fields
 			List<PatternDataField> tagDataFields = tag.getDataFields();
-			List<PatternDataField> groupNameDataFields = groupName.getDataFields();
-			
+			List<PatternDataField> groupNameDataFields = groupName
+					.getDataFields();
+
 			// replace 'X' in group name
 			for (int i = 0; i < groupNameDataFields.size(); i++) {
 				if (groupNameDataFields.get(i).isX()) {
 					groupNameDataFields.set(i, tagDataFields.get(i));
 				}
 			}
-			
+
 			return groupName.toString();
-			
+
 		} catch (ECSpecValidationException e) {
 			throw new ImplementationException(e.getMessage());
 		}
-		
+
 	}
-	
+
 	/**
 	 * This method returns a string representation of this pattern.
 	 * 
 	 * @return string representation
 	 */
 	public String toString() {
-		
+
 		StringBuffer buffer = new StringBuffer();
 		buffer.append(FIRST_FIELD);
 		buffer.append(":");
-		buffer.append(SECOND_FIELD);
+		buffer.append(secondField);
 		buffer.append(":");
 		buffer.append(thirdField);
 		buffer.append(":");
 		buffer.append(type.toSring());
 		buffer.append(":");
-		
+
 		for (PatternDataField dataField : dataFields) {
 			buffer.append(dataField.toString());
 			buffer.append(".");
 		}
-		
+
 		return buffer.substring(0, buffer.length() - 1);
-		
+
 	}
-	
+
 	//
 	// private methods
 	//
-	
+
 	/**
 	 * This method parses the data fields of this pattern.
 	 * 
-	 * @param dataFieldsString to parse
-	 * @param pattern the whole pattern
-	 * @throws ECSpecValidationException if the data field string is invalid
+	 * @param dataFieldsString
+	 *            to parse
+	 * @param pattern
+	 *            the whole pattern
+	 * @throws ECSpecValidationException
+	 *             if the data field string is invalid
 	 */
-	private void parseDataFields(String dataFieldsString, String pattern) throws ECSpecValidationException {
-		
+	private void parseDataFields(String dataFieldsString, String pattern)
+			throws ECSpecValidationException {
+
 		// split data fields
 		String[] dataFieldsStringArray = dataFieldsString.split("\\.");
-		
+
 		// check number of data fields
 		int nbrOfDataFields = type.getNumberOfDatafields();
 		if (dataFieldsStringArray.length < nbrOfDataFields) {
-			throw new ECSpecValidationException("Too less data fields '" + dataFieldsString + "' in pattern '" +
-					pattern +"'. Pattern Format '" + type + "' expects " + nbrOfDataFields + " data fields.");
+			throw new ECSpecValidationException("Too less data fields '"
+					+ dataFieldsString + "' in pattern '" + pattern
+					+ "'. Pattern Format '" + type + "' expects "
+					+ nbrOfDataFields + " data fields.");
 		} else if (dataFieldsStringArray.length > nbrOfDataFields) {
-			throw new ECSpecValidationException("Too many data fields '" + dataFieldsString + "' in pattern '" +
-					pattern + "'. Pattern Format '" + type + "' expects " + nbrOfDataFields + " data fields.");
+			throw new ECSpecValidationException("Too many data fields '"
+					+ dataFieldsString + "' in pattern '" + pattern
+					+ "'. Pattern Format '" + type + "' expects "
+					+ nbrOfDataFields + " data fields.");
 		}
-		
+
 		// check format of each field
 		for (int i = 0; i < dataFieldsStringArray.length; i++) {
-			dataFields.add(new PatternDataField(dataFieldsStringArray[i], usage));
+			dataFields
+					.add(new PatternDataField(dataFieldsStringArray[i], usage));
 		}
-		
+
 	}
-	
+
 	/**
-	 * This method indicates, if the needle string is an element of the haystack string array.
+	 * This method indicates, if the needle string is an element of the haystack
+	 * string array.
 	 * 
-	 * @param haystack string array which possibly contains the needle string
-	 * @param needle string to search in the haystack string array
+	 * @param haystack
+	 *            string array which possibly contains the needle string
+	 * @param needle
+	 *            string to search in the haystack string array
 	 * @return true if the haystack contains the needle and false otherwise
 	 */
 	private boolean containsString(String[] haystack, String needle) {
-		
+
 		if (needle == null) {
 			for (String element : haystack) {
 				if (element == null) {
@@ -381,7 +426,7 @@ public class Pattern {
 			}
 			return found;
 		}
-		
+
 	}
 
 }
